fb5311479a4ea4485f97df6adb9c737c1e47adee
pussy <sayam.hussain@gmail.com>
2016-11-26 16:19:48 +0000

    change config files & gemfile

--- config/environments.rb
+++ config/environments.rb
@@ -1,8 +1,34 @@
 require 'zlib'
 #The environment variable DATABASE_URL should be in the following format:
 # => postgres://{user}:{password}@{host}:{port}/path
-configure :production, :development, :test do
-	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://sayam:sayam@localhost:5432/development')
+configure :development, do
+	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://username:password@localhost:5432/development')
+
+	ActiveRecord::Base.establish_connection(
+			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+			:host     => db.host,
+			:username => db.user,
+			:password => db.password,
+			:database => db.path[1..-1],
+			:encoding => 'utf8'
+	)
+end
+
+configure :test do
+	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://username:password@localhost:5432/test')
+
+	ActiveRecord::Base.establish_connection(
+			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+			:host     => db.host,
+			:username => db.user,
+			:password => db.password,
+			:database => db.path[1..-1],
+			:encoding => 'utf8'
+	)
+end
+
+configure :production do
+	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://username:password@localhost:5432/production')
 
 	ActiveRecord::Base.establish_connection(
 			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
