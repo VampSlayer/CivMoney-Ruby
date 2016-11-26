f9cf2ca6c58abbcdc2b074e329eb67c8bbb5548f
pussy <sayam.hussain@gmail.com>
2016-11-26 13:07:33 +0000

    add missing migrate folder

---
+++ db/migrate/20161118234152_create_transaction.rb
@@ -0,0 +1,15 @@
+class CreateTransaction < ActiveRecord::Migration
+  def up
+  	create_table :transactions do |t|
+                t.date :date
+                t.decimal :amount
+  		          t.text :description
+                t.belongs_to :user, index: true
+                t.timestamps
+  	end
+  end
+
+  def down
+  	drop_table :transactions
+  end
+end
---
+++ db/migrate/20161118234158_create_user.rb
@@ -0,0 +1,14 @@
+class CreateUser < ActiveRecord::Migration
+  def up
+  	create_table :users do |t|
+                t.string :username
+                t.string :password_hash
+                t.string :salt
+                t.timestamps
+  	end
+  end
+
+  def down
+  	drop_table :users
+  end
+end
