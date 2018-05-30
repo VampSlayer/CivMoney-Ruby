b08f02c3b095aeef878656d8895f59ef723658af
Sayam Hussain <Sayam.hussain2008@gmail.com>
2018-05-30 15:59:57 +0100

    Update Dates.js

--- civmoneyreact/src/Components/Dates.js
+++ civmoneyreact/src/Components/Dates.js
@@ -60,3 +60,13 @@ export function getTodaysMonthName(){
  var months = {1:"January", 2:"Febuary", 3:"March", 4:"April", 5:"May", 6:"June", 7:"July", 8:"August", 9:"September", 10:"October", 11:"November", 12:"December"};
 return months[today];
 }
+
+export function getMonths(){
+  return ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+}
+
+export function getMonthNumber(month){
+ var months = {"January":1, "Febuary":2, "March":3, "April":4, "May":5, "June":6, "July":7, "August":8, "September":9, "October":10, "November":11, "December":12};
+return months[month];
+}
+
